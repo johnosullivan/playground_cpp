a9db96430ba89b2d9c439325725abd48ba2dd00a
John O'Sullivan <jnosullivan@icloud.com>
2019-03-26 19:13:25 -0500

    linking issue

--- main.cpp
+++ main.cpp
@@ -1,40 +1,64 @@
 #include <iostream>
 #include <string>
 #include <typeinfo>
-#include <vector> 
+#include <vector>
 #include <optional>
 #include <functional>
+#include <sstream>
+
+std::string VERSION_NAME = "v0.1.0";
 
 /*
- * Included the JSON for Modern C++ 
+ * Included the JSON for Modern C++
  * MIT: https://github.com/nlohmann/json
  */
 #include "json.hpp"
 
-//using namespace std;
-
-void processRequest() {
-
-
-}
-
 std::optional<int> string_to_int(std::string rawValue, int base) {
+    /* alloc size_type */
     std::string::size_type size;
+    /* try/catch stoi */
     try {
+        /* return optional with the correct value */
         return std::stoi(rawValue, &size, base);
     } catch (...) { }
+    /* return optional with no value */
     return { };
 }
 
+std::optional<double> string_to_double(std::string rawValue) {
+    /* try/catch stoi */
+    try {
+        /* return optional with the correct value */
+        return std::atof(rawValue.c_str());
+    } catch (...) { }
+    /* return optional with no value */
+    return { };
+}
+
+std::optional<std::vector<std::string>> string_to_vector(std::string rawValue) {
+    /* creates a stringstream and result vector  */
+    std::vector<std::string> result;
+    std::stringstream stream(rawValue);
+    /* while to iter over stream */
+    while(stream.good()) {
+        /* grabs the substr and add to vector */
+        std::string substr;
+        getline(stream, substr, ',');
+        result.push_back(substr);
+    }
+    /* returns the vector wrapped in optional */
+    return result;
+}
+
+
 int main() {
-    std::cout << "Starting main()" << std::endl;
+    std::cout << "Starting Playground++ Version ";
+    std::cout << VERSION_NAME << std::endl;
+
+    /* Started websockets */
     
-    std::cout << string_to_int("127", 0).value() << std::endl;
 
-    std::cout << string_to_int("127 North Ave", 0).value() << std::endl;
-
-    std::cout << string_to_int("1111111", 2).value() << std::endl;
-
-    std::cout << "Ending main()" << std::endl;
+    std::cout << "Started Playground++" << std::endl;
     return 0;
-} 
+}
