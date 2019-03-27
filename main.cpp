799a50cfac4a947d6d3aef09131aae95722fd122
John O'Sullivan <jnosullivan@icloud.com>
2019-03-26 23:39:36 -0500

    echoing working

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 #include <optional>
 #include <functional>
 #include <sstream>
+#include <set>
+#include <thread> 
 
 std::string VERSION_NAME = "v0.1.0";
 
@@ -12,16 +14,24 @@ std::string VERSION_NAME = "v0.1.0";
  * Included the JSON for Modern C++
  * MIT: https://github.com/nlohmann/json
  */
-#include "json.hpp"
+#include <nlohmann/json.hpp>
 
+/*
+ * Incldued websocketpp for all the websockets (RFC6455) 
+ * BSD: https://github.com/zaphoyd/websocketpp 
+ */
 #include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/server.hpp>
 
-typedef websocketpp::server<websocketpp::config::asio> server;
+#include "sole.hpp"
 
 using websocketpp::lib::placeholders::_1;
 using websocketpp::lib::placeholders::_2;
 using websocketpp::lib::bind;
+
+typedef std::set<websocketpp::connection_hdl,std::owner_less<websocketpp::connection_hdl>> connection_set;
+typedef websocketpp::server<websocketpp::config::asio> server;
+connection_set connections;
 
 // pull out the type of messages sent by our config
 typedef server::message_ptr message_ptr;
@@ -63,35 +73,54 @@ std::optional<std::vector<std::string>> string_to_vector(std::string rawValue) {
     return result;
 }
 
-// Define a callback to handle incoming messages
 void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
-    std::cout << "on_message called with hdl: " << hdl.lock().get()
-              << " and message: " << msg->get_payload()
-              << std::endl;
+    std::cout << "HDL: " << hdl.lock().get() << " PAYLOAD: " << msg->get_payload() << std::endl;
 
-    // check for a special command to instruct the server to stop listening so
-    // it can be cleanly exited.
-    if (msg->get_payload() == "stop-listening") {
-        s->stop_listening();
-        return;
-    }
 
     try {
         s->send(hdl, msg->get_payload(), msg->get_opcode());
     } catch (websocketpp::exception const & e) {
-        std::cout << "Echo failed because: "
-                  << "(" << e.what() << ")" << std::endl;
+        std::cout << "ERROR: " << "(" << e.what() << ")" << std::endl;
     }
 }
+
+void on_open(server* s, websocketpp::connection_hdl hdl) {
+    std::cout << "OPEN: " << hdl.lock().get() << std::endl;
+    connections.insert(hdl);
+}
+
+void on_close(server* s, websocketpp::connection_hdl hdl) {
+    std::cout << "CLOSE: " << hdl.lock().get() << std::endl;
+    connections.erase(hdl);
+}
+
+server echo_server;
+
+void while_loop_send(int x) 
+{ 
+    while (true) { 
+        std::cout << connections.size() << std::endl;
+
+        connection_set::iterator it;
+
+        std::stringstream ss;
+        ss << sole::uuid4();
+
+        for (it = connections.begin(); it != connections.end(); ++it) {
+            echo_server.send(*it, ss.str(), websocketpp::frame::opcode::text);
+        }
+
+        std::this_thread::sleep_for (std::chrono::milliseconds(500));
+    } 
+} 
 
 int main() {
     std::cout << "Starting Playground++ Version ";
     std::cout << VERSION_NAME << std::endl;
 
     /* Started websockets */
-    std::cout << "Started Playground++" << std::endl;
 
-    server echo_server;
+    std::thread tp(while_loop_send, 0);
 
     try {
         echo_server.set_access_channels(websocketpp::log::alevel::all);
@@ -102,14 +131,18 @@ int main() {
 
         echo_server.set_message_handler(bind(&on_message,&echo_server,::_1,::_2));
 
-        echo_server.listen(9002);
+        echo_server.set_open_handler(bind(&on_open,&echo_server,::_1));
+
+        echo_server.set_close_handler(bind(&on_close,&echo_server,::_1));
+
+        echo_server.listen(9000);
 
         echo_server.start_accept();
 
         echo_server.run();
     } catch (websocketpp::exception const & e) {
         std::cout << e.what() << std::endl;
-    } catch (...) {
-        std::cout << "other exception" << std::endl;
-    }
+    } catch (...) { }
+    
+    return 0; 
 }
