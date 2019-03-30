05b4b944a9ca31af83cd0d837afcaf02afdaab77
John O'Sullivan <jnosullivan@icloud.com>
2019-03-29 21:25:24 -0500

    countdown

--- main.cpp
+++ main.cpp
@@ -34,10 +34,14 @@ using websocketpp::lib::bind;
 typedef std::set<websocketpp::connection_hdl,std::owner_less<websocketpp::connection_hdl>> connection_set;
 typedef websocketpp::server<websocketpp::config::asio> server;
 typedef server::message_ptr message_ptr;
-connection_set connections;
 
+const std::string connection_confirm_frame = "CONNECTION_CONFIRM_FRAME";
+
+connection_set all_connections;
+std::map<std::string,connection_set> sessions;
 
 class Session {
+  private:
     std::string uuid;
     connection_set players;
 
@@ -96,42 +100,95 @@ std::optional<std::vector<std::string>> string_to_vector(std::string rawValue) {
     return result;
 }
 
+/*
+ * Will take two connections and check if they are the same 
+ */
+bool connections_equal(websocketpp::connection_hdl t, websocketpp::connection_hdl u)
+{
+    return !t.owner_before(u) && !u.owner_before(t);
+}
+
+/*
+ * Will fire as messages are coming in via the ws connection
+ */
 void on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg) {
     std::cout << "HDL: " << hdl.lock().get() << " PAYLOAD: " << msg->get_payload() << std::endl;
-
     try {
-        //s->send(hdl, msg->get_payload(), msg->get_opcode());
-        json payload = json::parse(msg->get_payload());
-        //std::cout << "payload: " << payload["session_id"] << std::endl;
+        //s->send(hdl, msg->get_payload(), msg->get_opcode()); // Basic Echo Example
 
-        /*for (it = connections.begin(); it != connections.end(); ++it) {
-            s->send(*it, msg->get_payload(), msg->get_opcode());
-        }*/
-    } catch (websocketpp::exception const & e) {
-        std::cout << "ERROR: " << "(" << e.what() << ")" << std::endl;
-    } catch (...) {
-        std::cout << "You done fucked up somewhere, johnny boi" << std::endl;
+        /*
+        json payload = json::parse(msg->get_payload());
+        std::string id = payload["session_id"];
+
+        connection_set xx = sessions[id];
+        connection_set::iterator it;
+        for (it = xx.begin(); it != xx.end(); ++it) {
+            if (!connections_equal(*it,hdl)) {
+                s->send(*it, msg->get_payload(), msg->get_opcode());
+            }
+        }
+        */
+    } catch (websocketpp::exception const & e) { } catch (...) { }
+}
+
+std::optional<std::string> generate_connection_link(websocketpp::connection_hdl hdl) {
+    json link_response;
+    link_response["id"] = sole::uuid4().str();
+    return link_response.dump();
+}
+
+/*
+ * Will fire as new connections become opened
+ */
+void on_open(server* s, websocketpp::connection_hdl hdl) {
+    /* Adds the connection to the set of all connections */
+    all_connections.insert(hdl);
+
+    //server::connection_ptr con = s->get_con_from_hdl(hdl);
+
+    std::optional<std::string> link_optional = generate_connection_link(hdl);
+    if (link_optional.has_value()) {
+        //s->send(hdl, link_optional.value(), websocketpp::frame::opcode::text);
     }
 }
 
-void on_open(server* s, websocketpp::connection_hdl hdl) {
-    std::cout << "OPEN: " << hdl.lock().get() << std::endl;
-    connections.insert(hdl);
+/*
+ * Will fire as old connections are closed
+ */
+void on_close(server* s, websocketpp::connection_hdl hdl) {
+    all_connections.erase(hdl);
 }
 
-void on_close(server* s, websocketpp::connection_hdl hdl) {
-    std::cout << "CLOSE: " << hdl.lock().get() << std::endl;
-    connections.erase(hdl);
-}
+bool status = true;
+
+class count_down { 
+    public: 
+        void operator()(server &server_ref, connection_set sessions_players) 
+        {         
+                for(int i = 0; i < 10; i++)
+                {
+                    connection_set::iterator it;
+
+                    for (it = sessions_players.begin(); it != sessions_players.end(); ++it) {
+                        server_ref.send(*it, std::to_string((int)i), websocketpp::frame::opcode::text);
+                    }
+
+                    std::this_thread::sleep_for (std::chrono::seconds(1));
+                }
+        } 
+}; 
 
 void dispatcher_thread(server &server_ref) 
 { 
     while (true) { 
-        std::cout << connections.size() << std::endl;
-        connection_set::iterator it;
+        int count = all_connections.size();
+        int matches_enabled = count / 2;
 
-        for (it = connections.begin(); it != connections.end(); ++it) {
-            server_ref.send(*it, sole::uuid4().str(), websocketpp::frame::opcode::text);
+        if (matches_enabled == 1) {
+            std::thread t;
+            t = std::thread(count_down(), std::ref(server_ref), all_connections);
+            t.detach();
+            all_connections.clear();
         }
 
         std::this_thread::sleep_for (std::chrono::seconds(1));
@@ -142,14 +199,34 @@ int main() {
     std::cout << "Starting Playground++ Version ";
     std::cout << VERSION_NAME << std::endl;
 
+    /*
     Session session1;
     std::cout << session1.get_id() << std::endl;
+    */
+
+   /* std::thread th2(thread_obj(), 3); 
+    th2.detach();
+
+    std::cout <<  th2.joinable() << std::endl;
+
+
+    std::cout <<  th2.joinable() << std::endl;*/
+/*
+    std::thread t;
+    t = std::thread(thread_obj());
+    t.detach();
+
+    std::this_thread::sleep_for (std::chrono::seconds(5));
+
+    status = false;
+
+    std::this_thread::sleep_for (std::chrono::seconds(5));*/
 
     /* Started websockets */
     server ws_server;
 
-    //std::thread tp(dispatcher_thread, std::ref(ws_server));
-
+    std::thread tp(dispatcher_thread, std::ref(ws_server));
+    
     try {
         ws_server.set_access_channels(websocketpp::log::alevel::all);
 
